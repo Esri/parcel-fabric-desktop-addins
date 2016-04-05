3af03966ce2cc709488b321e0423f8fab78726aa
Tim Hodson <thodson@esri.com>
2016-04-05 15:20:07 -0700

    Added Sample Parcel Trace tool
    Added Sample Parcel Trace tool. Demonstrates how to leverage the
    TraceConstructor class and make a tool that leverages the tracing
    functionality, and the edit sketch, for use with the fabric. In this
    code sample the sketch geometry is used to search the fabric boundary
    lines and returns a report with information about the geometry of
    different types of lines found.

---
+++ CodeSamples/SampleParcelTraceTool/SampleParcelTraceTool/SampleTraceTool1.Designer.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Editor;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geometry;
+
+namespace SampleParcelTraceTool
+{
+  public partial class SampleTraceTool1
+  {
+    #region Tool input overriding methods
+    protected sealed override void OnMouseDown(MouseEventArgs arg)
+    {
+      if (IsShapeConstructorOkay(m_csc))
+        m_csc.OnMouseDown(mousebutton2int(arg), mouseshift2int(arg), arg.X, arg.Y);
+    }
+
+    protected sealed override void OnMouseMove(MouseEventArgs arg)
+    {
+      if (IsShapeConstructorOkay(m_csc))
+        m_csc.OnMouseMove(mousebutton2int(arg), mouseshift2int(arg), arg.X, arg.Y);
+    }
+
+    protected sealed override void OnMouseUp(MouseEventArgs arg)
+    {
+      if (IsShapeConstructorOkay(m_csc))
+        m_csc.OnMouseUp(mousebutton2int(arg), mouseshift2int(arg), arg.X, arg.Y);
+    }
+
+    protected sealed override void OnKeyDown(KeyEventArgs arg)
+    {
+      if (IsShapeConstructorOkay(m_csc))
+        m_csc.OnKeyDown((int)arg.KeyCode, keyshift2int(arg));
+    }
+    protected sealed override void OnKeyUp(KeyEventArgs arg)
+    {
+      if (IsShapeConstructorOkay(m_csc))
+        m_csc.OnKeyUp((int)arg.KeyCode, keyshift2int(arg));
+    }
+
+    protected sealed override bool OnContextMenu(int x, int y)
+    {
+      if (IsShapeConstructorOkay(m_csc))
+        return m_csc.OnContextMenu(x, y);
+      else return false;
+    }
+
+    protected sealed override void OnRefresh(int hDC)
+    {
+      if (IsShapeConstructorOkay(m_csc))
+        m_csc.Refresh(hDC);
+    }
+
+    //int translations
+    private int mousebutton2int(MouseEventArgs arg)
+    {
+      switch (arg.Button)
+      {
+        case MouseButtons.Left: return 1;
+        case MouseButtons.Right: return 2;
+        case MouseButtons.Left | MouseButtons.Right: return 3;
+        case MouseButtons.Middle: return 4;
+        case MouseButtons.Left | MouseButtons.Middle: return 5;
+        case MouseButtons.Middle | MouseButtons.Right: return 6;
+        case MouseButtons.Left | MouseButtons.Middle | MouseButtons.Right: return 7;
+        default: return 0;
+      }
+    }
+
+    private int mouseshift2int(MouseEventArgs arg)
+    {
+      int shift = (arg.Shift ? 1 : 0) +
+      (arg.Control ? 2 : 0) +
+      (arg.Alt ? 4 : 0);
+      return shift;
+    }
+
+    private int keyshift2int(KeyEventArgs arg)
+    {
+      switch (arg.KeyCode)
+      {
+        case Keys.None: return 0;
+        case Keys.Shift: return 1;
+        case Keys.Control: return 2;
+        case Keys.Shift | Keys.Control: return 3;
+        case Keys.Alt: return 4;
+        case Keys.Shift | Keys.Alt: return 5;
+        case Keys.Control | Keys.Alt: return 6;
+        case Keys.Shift | Keys.Control | Keys.Alt: return 7;
+        default: return 0;
+      }
+    }
+
+    private bool IsShapeConstructorOkay(IShapeConstructor m_csc)
+    {
+      if (m_csc == null)
+        return false;
+
+      if (m_csc.Enabled != true)
+        return false;
+
+      if (m_csc.Active)
+        return true;
+      else
+        m_csc.Activate();
+      return true;
+    }
+    #endregion
+
+    #region ISketchTool Members
+    void ISketchTool.AddPoint(IPoint point, bool Clone, bool allowUndo)
+    {
+      if (IsShapeConstructorOkay(m_csc))
+        m_csc.AddPoint(point, Clone, allowUndo);
+    }
+
+    IPoint ISketchTool.Anchor
+    {
+      get
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          return m_csc.Anchor;
+        else
+          return null;
+      }
+    }
+
+    double ISketchTool.AngleConstraint
+    {
+      get
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          return m_csc.AngleConstraint;
+        else
+          return 0;
+      }
+      set
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          m_csc.AngleConstraint = value;
+      }
+    }
+
+    esriSketchConstraint ISketchTool.Constraint
+    {
+      get
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          return m_csc.Constraint;
+        else
+          return esriSketchConstraint.esriConstraintNone;
+      }
+      set
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          m_csc.Constraint = value;
+      }
+    }
+
+    double ISketchTool.DistanceConstraint
+    {
+      get
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          return m_csc.DistanceConstraint;
+        else
+          return 0;
+      }
+      set
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          m_csc.DistanceConstraint = value;
+      }
+    }
+
+    bool ISketchTool.IsStreaming
+    {
+      get
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          return m_csc.IsStreaming;
+        else
+          return false;
+      }
+      set
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          m_csc.IsStreaming = value;
+      }
+    }
+
+    IPoint ISketchTool.Location
+    {
+      get
+      {
+        if (IsShapeConstructorOkay(m_csc))
+          return m_csc.Location;
+        else
+          return null;
+      }
+    }
+    #endregion
+
+  }
+
+}
---
+++ CodeSamples/SampleParcelTraceTool/SampleParcelTraceTool/SampleTraceTool1.cs
@@ -0,0 +1,172 @@
+﻿//Add-in provided import library references
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Editor;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geometry;
+
+//Added Esri references
+using ESRI.ArcGIS.CadastralUI;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.GeoDatabaseExtensions;
+
+//Added non-Esri references
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SampleParcelTraceTool
+{
+
+  /// <summary>
+  /// A construction tool for ArcMap Editor, using shape constructors
+  /// </summary>
+  public partial class SampleTraceTool1 : ESRI.ArcGIS.Desktop.AddIns.Tool, IShapeConstructorTool, ISketchTool
+  {
+    //Add-in wizard added these member variables
+    private IEditor3 m_editor;
+    private IEditEvents_Event m_editEvents;
+    private IEditEvents5_Event m_editEvents5;
+    private IEditSketch3 m_edSketch;
+    private IShapeConstructor m_csc;
+
+    //Added fabric member variables
+    private ICadastralEditor m_pCadEd;
+    private ICadastralFabric m_pCadFab;
+    private IFeatureClass m_pFabricLines;
+
+    public SampleTraceTool1()
+    {
+      // Get the editor
+      m_editor = ArcMap.Editor as IEditor3;
+      m_editEvents = m_editor as IEditEvents_Event;
+      m_editEvents5 = m_editor as IEditEvents5_Event;
+    }
+
+    protected override void OnUpdate()
+    {
+      Enabled = ArcMap.Application != null;
+    }
+
+    protected override void OnActivate()
+    {
+      //get the cadastral editor and target fabric
+      m_pCadEd = (ICadastralEditor)ArcMap.Application.FindExtensionByName("esriCadastralUI.CadastralEditorExtension");
+      m_pCadFab = m_pCadEd.CadastralFabric;
+
+      if (m_pCadFab == null)
+      {
+        MessageBox.Show("No target fabric found. Please add a fabric to the map start editing, and try again.");
+        return;
+      }
+
+      m_pFabricLines = (IFeatureClass)m_pCadFab.get_CadastralTable(esriCadastralFabricTable.esriCFTLines);
+
+      m_editor.CurrentTask = null;
+      m_edSketch = m_editor as IEditSketch3;
+      m_edSketch.GeometryType = esriGeometryType.esriGeometryPolyline;
+      m_csc = new TraceConstructorClass();
+      m_csc.Initialize(m_editor);
+      m_edSketch.ShapeConstructor = m_csc;
+      m_csc.Activate();
+
+      // Setup events
+      m_editEvents.OnSketchModified += OnSketchModified;
+      m_editEvents5.OnShapeConstructorChanged += OnShapeConstructorChanged;
+      m_editEvents.OnSketchFinished += OnSketchFinished;
+    }
+
+    protected override bool OnDeactivate()
+    {
+      m_editEvents.OnSketchModified -= OnSketchModified;
+      m_editEvents5.OnShapeConstructorChanged -= OnShapeConstructorChanged;
+      m_editEvents.OnSketchFinished -= OnSketchFinished;
+      return true;
+    }
+
+    protected override void OnDoubleClick()
+    {
+      if (m_edSketch.Geometry == null)
+        return;
+
+      if (Control.ModifierKeys == Keys.Shift)
+      {
+        // Finish part
+        ISketchOperation pso = new SketchOperation();
+        pso.MenuString_2 = "Finish Sketch Part";
+        pso.Start(m_editor);
+        m_edSketch.FinishSketchPart();
+        pso.Finish(null);
+      }
+      else
+        m_edSketch.FinishSketch();
+    }
+
+    private void OnSketchModified()
+    {
+      if (IsShapeConstructorOkay(m_csc))
+        m_csc.SketchModified();
+    }
+
+    private void OnShapeConstructorChanged()
+    {
+      // Activate a new constructor
+      if (m_csc != null)
+        m_csc.Deactivate();
+      m_csc = null;
+      m_csc = m_edSketch.ShapeConstructor;
+      if (m_csc != null)
+        m_csc.Activate();
+    }
+
+    private void OnSketchFinished()
+    {
+      //Use the geometry from the sketch and make a narrow buffer to search for fabric lines contained within the buffer
+      IBufferConstruction pBuffConst = new BufferConstruction();
+      IGeometry pGeom = pBuffConst.Buffer(m_edSketch.Geometry, 0.1);
+      ISpatialFilter pSpatFilter = new SpatialFilter();
+      pSpatFilter.Geometry = pGeom;
+      pSpatFilter.SpatialRel = esriSpatialRelEnum.esriSpatialRelContains;
+
+      //set up for reporting information about the lines
+      string sReportString = "";
+      int iTotalLineCount = 0;
+      int iCurveCount = 0;
+      int iMultiSegmentCount = 0;
+
+      //Create a feature cursor to query the fabric lines table
+      IFeatureCursor pFeatCurs = m_pFabricLines.Search(pSpatFilter, false);
+      IFeature pFeat = pFeatCurs.NextFeature();
+
+      while (pFeat != null)
+      {
+        //loop through the found lines
+        //count all the lines
+        iTotalLineCount++;
+        ISegmentCollection pSegColl = (ISegmentCollection)pFeat.Shape;
+        if (pSegColl.SegmentCount > 1) //count lines that have more than 1 segment
+          iMultiSegmentCount++;
+        else
+        {
+          if (pSegColl.get_Segment(0) is ICircularArc)
+            iCurveCount++; //count lines that have a single circular arc geometry segment
+        }
+        Marshal.ReleaseComObject(pFeat);
+        pFeat = pFeatCurs.NextFeature();
+      }
+      Marshal.ReleaseComObject(pFeatCurs);
+
+      //report the results
+      sReportString += "\nTotal parcel lines: " + iTotalLineCount.ToString();
+      sReportString += "\nCurve parcel lines: " + iCurveCount.ToString();
+      sReportString += "\nMulti segment parcel lines: " + iMultiSegmentCount.ToString();
+
+      MessageBox.Show(sReportString, "Sample: Trace Report");
+    }
+
+  }
+
+}
